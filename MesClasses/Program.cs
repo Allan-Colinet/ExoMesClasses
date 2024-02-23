195e115cfb14bfb4e468746ce26d3d291454a178
allan colinet <allan.colinet@gmail.com>
2024-02-23 16:20:37 +0100

    Ajout d'un perso et test

--- MesClasses/Program.cs
+++ MesClasses/Program.cs
@@ -5,7 +5,16 @@
         static void Main(string[] args)
         {
             Guerrier jeanHubert = new Guerrier();
-            
+            jeanHubert.NomPerso = "BlaBla";
+            jeanHubert.NewStats();
+
+            Console.WriteLine($"Force de pédé : {jeanHubert.Force}");
+            Console.WriteLine($"Endurance de malade : {jeanHubert.Endurance}");
+            Console.WriteLine($"Intelligence de fou furieux : {jeanHubert.Intelligence}");
+            Console.WriteLine($"Sagesse de Shaolin : {jeanHubert.Sagesse}");
+            Console.WriteLine();
+            Console.WriteLine("Are You Ready ?");
+            Console.WriteLine("Fight !");
         }
     }
 }
