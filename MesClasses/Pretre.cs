1c4d904a3c249f4468a40323c662451ebffee91d
Allan-Colinet <Allan Colinet@DESKTOP-K2N2BAJ>
2024-02-23 16:07:49 +0100

    Ajoutez des fichiers projet.

---
+++ MesClasses/Pretre.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MesClasses
+{
+    internal class Pretre
+    {
+        public string NomPerso { get; set; }
+
+
+        private int _force;
+        public int Force
+        {
+            get { return _force; }
+            set { _force = value; }
+        }
+
+        private int _endurance;
+        public int Endurance
+        {
+            get { return _endurance; }
+            set { _endurance = value; }
+        }
+        private int _intelligence;
+        public int Intelligence
+        {
+            get { return _intelligence; }
+            set { _intelligence = value+2; }
+        }
+        private int _sagesse;
+        public int Sagesse
+        {
+            get { return _sagesse; }
+            set { _sagesse = value+4; }
+        }
+
+
+        public void NewStats()
+        {
+            Random rnd = new Random();
+            _force = rnd.Next(10, 21);
+            _endurance = rnd.Next(10, 21);
+            _intelligence = rnd.Next(10, 21);
+            _sagesse = rnd.Next(10, 21);
+        }
+    }
+}
---
+++ MesClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MesClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guerrier jeanHubert = new Guerrier();
+            
+        }
+    }
+}
